ed97487c6769ce3b107cd9a8071407b626dd2456
auriceliof <auricelio.suporte@hotmail.com>
2023-11-23 14:15:00 -0300

    06-07 Teste inserindo produto com dados válidos

--- dscommerce-backend-aulas-cap05/src/test/java/com/devsuperior/dscommerce/controllers/it/ProductControllerIt.java
+++ dscommerce-backend-aulas-cap05/src/test/java/com/devsuperior/dscommerce/controllers/it/ProductControllerIt.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.controllers.it;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -12,7 +13,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Category;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.tests.TokenUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,12 +30,38 @@ public class ProductControllerIt {
 	@Autowired
 	private MockMvc mockMvc;
 
-	private String productName;
+	@Autowired
+	private TokenUtil tokenUtil;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	private String clientToken, adminToken, invalidToken;
+	private String clientUsername, clientPassword, adminUsername, adminPassword; 
+	
+	private String productName;		
+	
+	private Product product;
+	private ProductDTO productDTO;
 
 	@BeforeEach
-	void setU() {
+	void setU() throws Exception {
+		
+		clientUsername = "maria@gmail.com";
+		clientPassword = "123456";
+		adminUsername = "alex@gmail.com";
+		adminPassword = "123456";
 
 		productName = "Macbook";
+		
+		clientToken = tokenUtil.obtainAccessToken(mockMvc, clientUsername, clientPassword);
+		adminToken = tokenUtil.obtainAccessToken(mockMvc, adminUsername, adminPassword);
+		invalidToken = adminToken + "xpto"; // Simulates wrong password
+		
+		Category category = new Category(2L, "Eletro");
+		product = new Product(null, "Console PlayStation 5", "Lorem ipsum dolor sit amet, consectetur adipiscing elit", 3999.90, "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg");
+		product.getCategories().add(category);
+		productDTO = new ProductDTO(product);
 	}
 
 	@Test
@@ -57,6 +91,29 @@ public class ProductControllerIt {
 		result.andExpect(jsonPath("$.content[0].price").value(1250.0));
 		result.andExpect(jsonPath("$.content[0].imgUrl").value("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
 	}
+		
+	@Test
+	public void insertShouldReturnProductDTOCreatedWhenLoggedAsAdmin() throws Exception {
+		
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+		
+		ResultActions result = 
+				mockMvc.perform(post("/products")
+					.header("Authorization", "Bearer " + adminToken)
+					.content(jsonBody)
+					.contentType(MediaType.APPLICATION_JSON)
+					.accept(MediaType.APPLICATION_JSON))
+					.andDo(MockMvcResultHandlers.print());
+		
+		result.andExpect(status().isCreated());
+		result.andExpect(jsonPath("$.id").value(26L));
+		result.andExpect(jsonPath("$.name").value("Console PlayStation 5"));
+		result.andExpect(jsonPath("$.description").value("Lorem ipsum dolor sit amet, consectetur adipiscing elit"));
+		result.andExpect(jsonPath("$.price").value(3999.90));
+		result.andExpect(jsonPath("$.imgUrl").value("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"));
+		result.andExpect(jsonPath("$.categories[0].id").value(2L));
+	}
+	
 }
 
 

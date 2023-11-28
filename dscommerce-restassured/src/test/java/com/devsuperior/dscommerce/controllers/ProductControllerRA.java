15460e084d178fec0b6dbc9b4025e6aa326b589c
auriceliof <auricelio.suporte@hotmail.com>
2023-11-28 15:17:15 -0300

    06-18 Teste busca de produto por nome

--- dscommerce-restassured/src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
+++ dscommerce-restassured/src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -13,11 +13,14 @@ import org.junit.jupiter.api.Test;
 public class ProductControllerRA {
 	
 	private Long existingProductId, nonExistingProductId, dependentProductId;
+	private String productName;
 
 	@BeforeEach
 	private void setup() {
 		
 		baseURI = "http://localhost:8080";
+		
+		productName = "Macbook";
 	}		
 	
 	@Test
@@ -35,7 +38,47 @@ public class ProductControllerRA {
 			.body("categories.id", hasItems(2, 3))
 			.body("categories.name", hasItems("Eletrônicos", "Computadores"));
 	}
+	
+	@Test
+	public void findAllShouldReturnPagedProductsWhenProductNameParamIsEmpty() {
+		given()
+			.get("/products?page=0")
+		.then()
+			.body("content.name", hasItems("Macbook Pro", "PC Gamer Tera"));
+	}
+	
+	@Test
+	public void findAllShouldReturnPagedProductsWhenProductNameParamIsNotEmpty() {
+		given()
+			.get("/products?name={productName}", productName)
+		.then()
+			.statusCode(200)
+			.body("content.id[0]", is(3))
+			.body("content.name[0]", equalTo("Macbook Pro"))
+			.body("content.price[0]", is(1250.0f))
+			.body("content.imgUrl[0]", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
+	}
+	
+	@Test
+	public void findAllShouldReturnPagedProductsWithPriceGreaterThan2000() {
+		given()
+			.get("/products?size=25")
+		.then()
+			.body("content.findAll { it.price > 2000 }.name", hasItems("Smart TV", "PC Gamer Weed"));
+	}	
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

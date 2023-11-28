aeb0b9b98f01fa1779f51b5623f5393c5cdb17c0
auriceliof <auricelio.suporte@hotmail.com>
2023-11-28 14:25:03 -0300

    06-17 Teste busca de produto por id

---
+++ dscommerce-restassured/src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -0,0 +1,52 @@
+package com.devsuperior.dscommerce.controllers;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasItems;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class ProductControllerRA {
+	
+	private Long existingProductId, nonExistingProductId, dependentProductId;
+
+	@BeforeEach
+	private void setup() {
+		
+		baseURI = "http://localhost:8080";
+	}		
+	
+	@Test
+	public void findByIdShouldReturnProductWhenIdExists() {
+		existingProductId = 2L;
+		
+		given()
+			.get("/products/{id}", existingProductId)
+		.then()
+			.statusCode(200)
+			.body("id", is(2))
+			.body("name", equalTo("Smart TV"))
+			.body("imgUrl", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"))
+			.body("price", is(2190.0F))
+			.body("categories.id", hasItems(2, 3))
+			.body("categories.name", hasItems("Eletrônicos", "Computadores"));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
